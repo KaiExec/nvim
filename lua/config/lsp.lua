79ba7ad68f562a001c9d88c4b67b55bdf3cacf93
KaiExec <kaiexecutor@gmail.com>
2025-06-21 08:06:17 +0800

    re-config the lsp server(add the mason and lspconfig plugins, add the lsplist.lua)

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,37 +1,45 @@
-vim.lsp.enable 'lua_ls'
-
 vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('my.lsp', {}),
-  callback = function(args)
-      -- Get LSP client
-    local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-      -- Keybinding
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {buffer = args.buf, desc = 'LSP: Goto Definition'})
-      vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {buffer = args.buf, desc = 'LSP: Goto Declaration'})
-      -- Diagnostics
-      vim.diagnostic.config{
-          virtual_text = true,
-      }
-      vim.keymap.set('n', '<leader>ld',function()
-          vim.diagnostic.open_float { source = true }
-      end, {buffer = args.buf, desc = 'LSP: Goto Declaration'})
-      vim.keymap.set(
-          'n',
-          '<leader>td',
-          (function()
-              local diag_status = 1 -- 1 is show; 0 is hide
-              return function()
-                  if diag_status == 1 then
-                      diag_status = 0
-                      vim.diagnostic.config { underline = false, virtual_text = false, signs = false, update_in_insert = false }
-                  else
-                      diag_status = 1
-                      vim.diagnostic.config { underline = true, virtual_text = true, signs = true, update_in_insert = true }
-                  end
-              end
-          end)(),
-          { buffer = args.buf, desc = 'LSP: Toggle diagnostics display' }
-      )
-
-  end,
+    group = vim.api.nvim_create_augroup('my.lsp', {}),
+    callback = function(args)
+        -- Get LSP client
+        local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+        -- Keybinding
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = args.buf, desc = 'LSP: Goto Definition' })
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { buffer = args.buf, desc = 'LSP: Goto Declaration' })
+        vim.keymap.set('n', 'rn', vim.lsp.buf.rename, { buffer = args.buf, desc = 'LSP: Rename' })
+        -- Diagnostics
+        vim.diagnostic.config {
+            virtual_text = true,
+        }
+        -- InlayHint if passible
+        if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
+            vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = args.buf })
+        end
+        -- Diagnostics Line
+        vim.keymap.set('n', '<leader>ld', function()
+            vim.diagnostic.open_float { source = true }
+        end, { buffer = args.buf, desc = 'LSP: Goto Declaration' })
+        -- Toggle Diagnostics
+        vim.keymap.set(
+            'n',
+            '<leader>td',
+            (function()
+                local diag_status = 1 -- 1 is show; 0 is hide
+                return function()
+                    if diag_status == 1 then
+                        diag_status = 0
+                        vim.diagnostic.config { underline = false, virtual_text = false, signs = false, update_in_insert = false }
+                    else
+                        diag_status = 1
+                        vim.diagnostic.config { underline = true, virtual_text = true, signs = true, update_in_insert = true }
+                    end
+                end
+            end)(),
+            { buffer = args.buf, desc = 'LSP: Toggle diagnostics display' }
+        )
+        -- Format code
+        vim.keymap.set('n', '<leader><leader>f', function()
+            vim.lsp.buf.format({ async = false })
+        end, { buffer = args.buf, desc = 'LSP: Goto Definition' })
+    end,
 })
--- lua/plugins/provider.lua
+++ lua/plugins/provider.lua
@@ -1,29 +1,25 @@
 return
+{
     {
-        {
-            -- TREESITTER
-            "nvim-treesitter/nvim-treesitter",
-            branch = 'master',
-            lazy = false,
-            build = ":TSUpdate",
+        -- TREESITTER
+        "nvim-treesitter/nvim-treesitter",
+        branch = 'master',
+        lazy = false,
+        build = ":TSUpdate",
 
-            main = "nvim-treesitter.configs",
-            opts = {
-                -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-                ensure_installed = { "c", "lua", "markdown"},
-                -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-                auto_install = true,
+        main = "nvim-treesitter.configs",
+        opts = {
+            -- A list of parser names, or "all" (the listed parsers MUST always be installed)
+            ensure_installed = { "c", "lua", "markdown"},
+            -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+            auto_install = true,
 
-                ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-                -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
-                highlight = {
-                    enable = true
-                },
+            ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+            -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+            highlight = {
+                enable = true
             },
-            -- key = {
-            --     { "n", }
-            --
-            -- }
+        },
         },
         {
             -- WHICH-KEY
@@ -44,7 +40,12 @@ return
             },
         },
         {
-            "mason-org/mason.nvim",
-            opts = {}
+            "neovim/nvim-lspconfig",
+            dependencies = {
+                "mason-org/mason.nvim",
+            },
+            config = function()
+                require("mason").setup()
+            end,
         }
     }
