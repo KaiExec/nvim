99b9c7df4eea160e4df9bbf77f811f0d4ee3e576
KaiExec <104556222@gmail.com>
2025-12-10 13:33:12 +0800

    Add a plugin, Add 2 LSP
    Add zk-nvim, Add golsp and zk LSP

---
+++ lua/plugins/zk.lua
@@ -0,0 +1,9 @@
+return {
+	"zk-org/zk-nvim",
+	main = "zk",
+	opts = {
+		-- Can be "telescope", "fzf", "fzf_lua", "minipick", "snacks_picker",
+		-- or select" (`vim.ui.select`).
+		picker = "snacks_picker",
+	},
+}
