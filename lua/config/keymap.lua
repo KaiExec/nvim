8b3899eda2034b5a8a131ae4646705e358d3c8b5
KaiExec <104556222@gmail.com>
2025-12-13 00:16:49 +0800

    Add a comment
    Add a comment in keymap.lua for figure out the usage of keymap

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -1,12 +1,13 @@
 function Map(mode, lhs, rhs, opts)
-    local options = { noremap = true, silent = true }
-    if opts then
-        options = vim.tbl_extend("force", options, opts)
-    end
-    vim.keymap.set(mode, lhs, rhs, options)
+	local options = { noremap = true, silent = true }
+	if opts then
+		options = vim.tbl_extend("force", options, opts)
+	end
+	vim.keymap.set(mode, lhs, rhs, options)
 end
 
-Map({"n", "v"}, "J", "5j")
-Map({"n", "v"}, "K", "5k")
-Map({"n", "v", "o"}, "H", "^")
-Map({"n", "v", "o"}, "L", "$")
+-- Move
+Map({ "n", "v" }, "J", "5j")
+Map({ "n", "v" }, "K", "5k")
+Map({ "n", "v", "o" }, "H", "^")
+Map({ "n", "v", "o" }, "L", "$")
