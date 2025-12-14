042886bf1854a6ef7de834b01c04ee22b905d4cd
KaiExec <104556222@gmail.com>
2025-12-14 14:21:08 +0800

    Add the support to format json file automatically

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -6,6 +6,7 @@ return {
 			python = { "isort", "black" },
 			javascript = { "prettierd", "prettier", stop_after_first = true },
 			typescript = { "prettierd", "prettier", stop_after_first = true },
+			json = { "prettierd", "prettier", stop_after_first = true },
 		},
 		format_on_save = {
 			-- These options will be passed to conform.format()
